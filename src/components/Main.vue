b1a5bf97a27331250d4d551f51329d0e4245101e
Jack-rm <jackcnsr94@gmail.com>
2021-09-24 16:32:12 +0200

    work on setting up layout

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,6 +1,20 @@
 <template>
   <div>
-    <h3>Main</h3>
+    <main>
+     
+     <section class="my_content-section">
+        <div class="my_content">
+          <h3> --- Content goes here --- </h3>
+        </div>
+      </section>
+      
+      <section class="my_features-section">
+        <div class="my_features">
+          <h3> --- Features go here --- </h3>
+        </div>
+      </section>
+
+    </main>
   </div>
 </template>
 
@@ -13,5 +27,25 @@ export default {
 <style lang="scss">
 @import "../style/variables.scss";
 @import "../style/general.scss";
+
+main .my_content-section{
+  background-color: $darkerGrey;
+  padding: 50px 0px;
+}
+
+main .my_features-section{
+  background-color: $borderBottomActiveColor;
+  padding: 50px 0px;
+}
+
+.my_content{
+    padding: 0px 250px;
+    color: white;
+}
+
+.my_features{
+    margin: 0px 250px;
+    color: white;
+}
 
 </style>
