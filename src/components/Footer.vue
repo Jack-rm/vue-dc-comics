b102f560c33e2e11ab477fc1ffc24ca173c749b8
Jack-rm <jackcnsr94@gmail.com>
2021-09-24 16:53:44 +0200

    setting up Footer layout

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,7 +1,39 @@
 <template>
-  <div>
-    <h3>Footer</h3>
-  </div>
+  <footer>
+    <div class="my_footer-start">
+      <div class="my_footer-wrapper">
+        <div>
+          <p>colonna</p>
+        </div>
+        <div>
+          <p>colonna</p>
+        </div>
+        <div>
+          <p>colonna</p>
+        </div>
+
+        <div>
+          <img src="" alt="Big logo Dc Comics">
+        </div>
+      </div>
+    </div>
+
+    <div class="my_footer-end">
+      <div class="my_footer-wrapper">
+
+        <button>bottone</button>
+        
+        <div>
+          <p>Follow us</p>
+          <p>facebook</p>
+          <p>twitter</p>
+          <p>yt</p>
+          <p>pinterest</p>
+          <p>localizzazione</p>
+        </div>
+      </div>
+    </div>
+  </footer>
 </template>
 
 <script>
@@ -14,4 +46,18 @@ export default {
 @import "../style/variables.scss";
 @import "../style/general.scss";
 
+.my_footer-start{
+  background-image: url("../assets/img/footer-bg.jpg");
+  background-size: cover;
+  background-position: center;
+  background-repeat: no-repeat;
+}
+
+.my_footer-wrapper{
+    margin: 0px 250px;
+}
+
+.my_footer-end{
+  background-color: $myGrey;
+}
 </style>
