fac12849d9c954392b00ed6d3e4f0073bdd4cb6f
Jack-rm <jackcnsr94@gmail.com>
2021-09-26 17:23:47 +0200

    Added new 'features' component

--- src/components/Features.vue
+++ src/components/Features.vue
@@ -2,7 +2,14 @@
     <div>
         <section class="my_features-section">
             <div class="my_features">
-                <h3> --- Features go here --- </h3>
+                <ul>
+                    <li v-for="(link,index) in links" :key="index">
+                        <a href="#">
+                            <img :src="link.img"/>
+                            {{ link.text }}
+                        </a>
+                    </li>
+                </ul>
             </div>
         </section>
     </div>
@@ -10,6 +17,34 @@
 
 <script>
 export default {
+    name: 'Features',
+    data: function() {
+        return{
+            index:0,
+            links: [
+                {
+                    text: "Digital Comics",
+                    img: require("../assets/img/buy-comics-digital-comics.png")
+                },
+                {
+                    text: "DC Merchandise",
+                    img: require("../assets/img/buy-comics-merchandise.png")
+                },
+                {
+                    text: "Subscription",
+                    img: require("../assets/img/buy-comics-subscriptions.png")
+                },
+                {
+                    text: "Comic Shop Locator",
+                    img: require("../assets/img/buy-comics-shop-locator.png")
+                },
+                {
+                    text: "DC Power Visa",
+                    img: require("../assets/img/buy-dc-power-visa.svg")
+                },
+            ],
+        }
+    }
 
 }
 </script>
@@ -26,5 +61,24 @@ export default {
 .my_features{
     margin: 0px 250px;
     color: white;
+
+    & ul{
+        display: flex;
+        align-items: center;
+        justify-content: space-evenly;
+        list-style: none;
+    }
+
+    & ul li a{
+        text-decoration: none;
+        color: white;
+        display: flex;
+        align-items: center;
+    }
+
+    & ul li a img{
+        height: 45px;
+        margin-right: 20px;
+    }
 }
 </style>
